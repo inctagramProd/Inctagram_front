113fbbe9ba091c0c829b926db6b7aac2ad734310
Pavel-Vladimirovich <nesoglasnyj.pavel@gmail.com>
2023-12-07 19:59:13 +0300

    commit index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div
         style={{
           display: 'flex',
-          justifyContent: 'spaceBetween',
+          justifyContent: 'spaceBetweeNNN',
           marginTop: '50px',
           textAlign: 'center',
         }}
